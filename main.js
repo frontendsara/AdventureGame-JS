ec14722a3aa8f27cf04a6d74984de0c4ac87b998
frontendsara <sara.karlsson1991@hotmail.com>
2022-11-17 11:42:48 +0100

    added a background-image

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function startScene () {
     const text = document.getElementById("text");
     const button1 = document.getElementById("button1");
     const button2 = document.getElementById("button2");
+    
 
     text.textContent = "Hej! Du ska ut på äventyr i skogen eller till havet. Vad väljer du?";
 
